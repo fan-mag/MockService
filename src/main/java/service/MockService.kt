44b19f737b53674b5fc329b4e1900d78a97a8144
fan-mag <fan-mag@mail.ru>
2019-11-19 21:56:10 +0300

    Mock Service Complete

--- src/main/java/service/MockService.kt
+++ src/main/java/service/MockService.kt
@@ -6,77 +6,71 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.io.BufferedReader
 import java.io.FileInputStream
-import java.io.FileReader
 import java.util.*
-import javax.servlet.http.HttpServletMapping
 import javax.servlet.http.HttpServletRequest
 
 @SpringBootApplication
 @RestController
 open class MockService {
 
-
-    @RequestMapping("/mock",method = arrayOf(RequestMethod.GET))
-    fun mockService(@RequestBody(required = false) requestBody : String?,
-                    @RequestHeader requestHeaders : HttpHeaders,
+    @RequestMapping("/", method = arrayOf(RequestMethod.GET))
+    fun mockService(@RequestBody(required = false) requestBody: String?,
+                    @RequestHeader requestHeaders: HttpHeaders,
                     request: HttpServletRequest): ResponseEntity<Any> {
         val requestParameters = request.parameterMap
-
-        println()
+        loadResponse()
+        /* Mock Logic Start */
+        println("BODY: $requestBody")
+        requestHeaders.forEach { key, value -> println("HEADER: $key : $value") }
+        requestParameters.forEach { key, value -> println("PARAMS: $key : $value") }
+        /* Mock Logic End */
         return ResponseEntity(responseBody, responseHeaders, responseStatus)
     }
 
     companion object {
-
-        var requestBody = String()
-        val requestHeaders = HttpHeaders()
-        val requestParam = HashMap<String, String>()
-        val responseHeaders = HttpHeaders()
-        val responseStatus: HttpStatus = HttpStatus.OK
-        lateinit var responseBody: String
+        var responseHeaders = HttpHeaders()
+        var responseStatus = HttpStatus.OK
+        var responseBody = String()
 
         @JvmStatic
         fun main(args: Array<String>) {
-            loadRequestHeaders()
-            loadRequestParams()
-            loadRequestBody()
-            loadResponseBody()
             SpringApplication.run(MockService::class.java)
         }
 
-        fun loadResponseBody() {
-            val reader = FileInputStream("src/main/resources/response/body.txt")
+        fun loadResponse() {
+            responseStatus = loadResponseStatus()
+            responseHeaders = loadResponseHeaders()
+            responseBody = loadResponseBody()
+        }
+
+        fun loadResponseBody(): String {
+            val reader = FileInputStream("src/main/resources/response-body.txt")
             val fileSize = reader.available()
             val byteArray = ByteArray(fileSize)
             reader.read(byteArray)
-            responseBody = String(byteArray)
+            return String(byteArray)
         }
 
-        fun loadRequestBody() {
-            val reader = FileInputStream("src/main/resources/request/body.txt")
+        fun loadResponseHeaders(): HttpHeaders {
+            val properties = Properties()
+            properties.load(FileInputStream("src/main/resources/response-headers.properties"))
+            val headers = HttpHeaders()
+            properties.toMap().forEach { key, value ->
+                headers.add(key.toString(), value.toString())
+            }
+            return headers
+        }
+
+        fun loadResponseStatus(): HttpStatus {
+            val reader = FileInputStream("src/main/resources/response-statuscode.txt")
             val fileSize = reader.available()
             val byteArray = ByteArray(fileSize)
             reader.read(byteArray)
-            requestBody = String(byteArray)
+            val statusCode: Int = String(byteArray).toInt()
+            return HttpStatus.valueOf(statusCode)
         }
 
-        fun loadRequestHeaders() {
-            val getRequestHeadersProperties = Properties()
-            getRequestHeadersProperties.load(FileReader("src/main/resources/request/headers.properties"))
-            getRequestHeadersProperties.toMap().forEach { key, value ->
-                requestHeaders.add(key.toString(), value.toString())
-            }
-        }
-
-        fun loadRequestParams() {
-            val getRequestParamsProperties = Properties()
-            getRequestParamsProperties.load(FileReader("src/main/resources/request/params.properties"))
-            getRequestParamsProperties.toMap().forEach { key, value ->
-                requestParam.put(key.toString(), value.toString())
-            }
-        }
     }
 
 }
